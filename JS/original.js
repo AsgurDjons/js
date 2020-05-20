e5424898323bb9d306b76a98f2d73d97b4f0086f
Asgyr <asgyrdjons@gmail.com>
2020-05-20 22:24:10 +0300

    1

--- JS/original.js
+++ JS/original.js
@@ -23,12 +23,20 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] =b;
-personalMovieDB.movies[c] =d;
+for(let i =0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a !='' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] =b;
+        console.log('Done');
+    }else {
+        console.log('error');
+        i--;
+    }
+
+    personalMovieDB.movies[a] =b;
+}
 
 console.log(personalMovieDB);
