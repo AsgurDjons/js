77840b7da61e8dedeba09fe5d387c71001413468
Asgyr <asgyrdjons@gmail.com>
2020-05-30 14:40:27 +0300

    fich

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,11 @@
-"use strict";
+// "use strict";
 
 
-let number = 4.6;
-console.log(-4/0);
+// // let number = 4.6;
+// // console.log(-4/0);
+
+
+// const logg = "Hello world";
+// console.log(logg.slice(5, 8));
+
+// console.log(logg.substring(5, 8));
